c3accffb0d8b46c9aa7f93085a7b3c832287a008
Maciej Zurad <maciej.zurad@gmail.com>
2015-03-11 10:09:46 +0100

    Fixing bug with heroku netiface

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,8 @@
 class PagesController < ApplicationController
   def index
-	@local_ip = %x(ifconfig eth0).match(/inet addr:(\d*\.\d*\.\d*\.\d*)/)[1] || "localhost"
+
+  	eth_interface = %x(ifconfig eth0)
+	@local_ip = eth_interface.match(/inet addr:(\d*\.\d*\.\d*\.\d*)/)[1] if eth_interface || "localhost"
 	@local_ip = @local_ip + ':9090'
 	#@local_ip = "localhost:9090"
   end
